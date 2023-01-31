a33a4584ee903a23301cbab8ade10de94273b7b1
japygo <hyunnjinn@gmail.com>
2023-01-31 21:44:30 +0900

    Update 6.1.4

--- src/main/java/next/web/UpdateUserServlet.java
+++ src/main/java/next/web/UpdateUserServlet.java
@@ -10,13 +10,25 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet("/user/update")
 public class UpdateUserServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        req.setAttribute("user", DataBase.findUserById(req.getParameter("userId")));
+        String userId = req.getParameter("userId");
+        HttpSession session = req.getSession();
+        Object value = session.getAttribute("user");
+        if (value != null) {
+            User user = (User) value;
+            if (!user.getUserId().equals(userId)) {
+                resp.sendRedirect("/user/list");
+                return;
+            }
+        }
+
+        req.setAttribute("user", DataBase.findUserById(userId));
         RequestDispatcher rd = req.getRequestDispatcher("/user/update.jsp");
         rd.forward(req, resp);
     }
