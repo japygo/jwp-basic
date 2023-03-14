22517c74f3fb9d38131d16dc7347e761c71ce9e2
japygo <hyunnjinn@gmail.com>
2023-03-14 21:23:51 +0900

    Update 10.1.2.2

---
+++ src/test/java/core/ref/Junit3TestRunner.java
@@ -0,0 +1,17 @@
+package core.ref;
+
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+
+public class Junit3TestRunner {
+    @Test
+    public void run() throws Exception {
+        Class<Junit3Test> clazz = Junit3Test.class;
+        for (Method method : clazz.getMethods()) {
+            if (method.getName().startsWith("test")) {
+                method.invoke(clazz.getDeclaredConstructor().newInstance());
+            }
+        }
+    }
+}
